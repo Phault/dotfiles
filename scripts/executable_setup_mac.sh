4e3538e2b0de94c13d99616fd1214f1842aa3d7a
Casper Lindschouw <casper@lindschouw.me>
2023-08-27 12:57:12 +0200

    add setup script for mac

---
+++ scripts/executable_setup_mac.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Installing Homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+cd "$HOME"
+echo "Installing applications..."
+/opt/homebrew/bin/brew bundle
+
+echo "Setting up chezmoi..."
+/opt/homebrew/bin/chezmoi init phault/dotfiles --apply --prompt
+
+echo "Setup complete."
