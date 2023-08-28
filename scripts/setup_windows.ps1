37485cae9bfab7daf4413752b4742a92dbee5f98
Casper Lindschouw <casper@lindschouw.me>
2023-08-28 09:36:54 +0200

    rework setup_windows a bit

--- scripts/setup_windows.ps1
+++ scripts/setup_windows.ps1
@@ -8,16 +8,6 @@ function Verify-Elevated {
     return $myPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 }
 
-# Check to see if we are currently running "as Administrator"
-if (!(Verify-Elevated)) {
-    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
-    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
-    $newProcess.Verb = "runas";
-    [System.Diagnostics.Process]::Start($newProcess);
-    Write-Host -NoNewLine 'Press any key to continue...';
-    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-    exit
-}
 
 # Reload the $env object from the registry
 function Refresh-Environment {
@@ -36,33 +26,47 @@ function Refresh-Environment {
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 }
 
+function Setup-Windows {
+    Write-Host "Installing dependencies..."
+    winget install Git.Git --silent --accept-package-agreements --no-upgrade --overrides "/VerySilent /NoRestart /Components=""icons,assoc,assoc_sh,gitlfs"""
+    winget install twpayne.chezmoi --silent --accept-package-agreements --no-upgrade
 
-Write-Host "Installing dependencies..."
-winget install Git.Git --silent --accept-package-agreements --no-upgrade --overrides "/VerySilent /NoRestart /Components=""icons,assoc,assoc_sh,gitlfs"""
-winget install twpayne.chezmoi --silent --accept-package-agreements --no-upgrade
+    # we need scoop for age encryption unfortunately
+    # scoop installer looks for git in path
+    Refresh-Environment
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/scoopinstaller/install/master/install.ps1"))
 
-# we need scoop for age encryption unfortunately
-# scoop installer looks for git in path
-Refresh-Environment
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/scoopinstaller/install/master/install.ps1"))
+    # no need to refresh enviroment, as the installer adds it to the current session
+    scoop bucket add extras
+    scoop install age
 
-# no need to refresh enviroment, as the installer adds it to the current session
-scoop bucket add extras
-scoop install age
+    # uncertain if necessary for chezmoi to see age, but added just in case
+    Refresh-Environment
 
-# uncertain if necessary for chezmoi to see age, but added just in case
-Refresh-Environment
+    Write-Host "Setting up chezmoi..."
+    chezmoi init phault/dotfiles --apply
 
-Write-Host "Setting up chezmoi..."
-chezmoi init phault/dotfiles --apply
+    Write-Host "Configuring Windows..."
+    . "${env:UserProfile}/scripts/configure_windows.ps1"
 
-Write-Host "Configuring Windows..."
-. "${env:UserProfile}/scripts/configure_windows.ps1"
+    Write-Host "Installing apps..."
+    . "${env:UserProfile}/scripts/install_apps.ps1"
 
-Write-Host "Installing apps..."
-. "${env:UserProfile}/scripts/install_apps.ps1"
+    Write-Host "Setup complete."
 
-Write-Host "Setup complete."
+    # for good measure
+    Refresh-Environment
+}
 
-# for good measure
-Refresh-Environment
+if (Verify-Elevated) {
+    Setup-Windows
+    
+    # in case we relaunched as elevated, wait for input so user can read output
+    Write-Host -NoNewLine 'Press any key to continue...';
+    $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+} else {
+    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+    $newProcess.Verb = "runas";
+    [System.Diagnostics.Process]::Start($newProcess);
+}
