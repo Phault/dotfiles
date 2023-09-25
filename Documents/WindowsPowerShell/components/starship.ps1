a5efc047d9b5a15dc3ba50276b208302d2e2d851
Casper Lindschouw <casper@lindschouw.me>
2023-09-25 19:14:16 +0200

    send OSC 7 and set window title when running starship in powershell

--- Documents/WindowsPowerShell/components/starship.ps1
+++ Documents/WindowsPowerShell/components/starship.ps1
@@ -1,3 +1,16 @@
 if (Get-Command "starship" -ErrorAction SilentlyContinue) {
+  function Invoke-Starship-PreCommand {
+    $current_location = $executionContext.SessionState.Path.CurrentLocation
+    if ($current_location.Provider.Name -eq "FileSystem") {
+      $ansi_escape = [char]27
+      $provider_path = $current_location.ProviderPath -replace "\\", "/"
+      $host.ui.Write($prompt)
+      $host.ui.RawUI.WindowTitle = (Get-Item $pwd).Basename
+    }
+
+    $host.ui.Write($args)
+  }
+
+  $host.ui.RawUI.WindowTitle = (Get-Item $pwd).Basename
   Invoke-Expression (&starship init powershell)
 }
