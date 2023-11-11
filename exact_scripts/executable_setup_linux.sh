88162f0759cd50e7ad1f784a4d4eb064f0a11249
Casper Lindschouw <casper@lindschouw.me>
2023-11-11 18:39:58 +0100

    install npm, pip, dotnet and go tools

--- exact_scripts/executable_setup_linux.sh
+++ exact_scripts/executable_setup_linux.sh
@@ -7,16 +7,48 @@ echo "Installing Homebrew..."
 
 cd "$HOME"
 echo "Installing applications..."
-/opt/homebrew/bin/brew bundle
+/home/linuxbrew/.linuxbrew/bin/brew bundle
+
+# hard req on apt :(
+echo "Installing dotnet sdk..."
+sudo apt install -y dotnet-sdk-7.0
 
 echo "Setting up chezmoi..."
-/opt/homebrew/bin/chezmoi init phault/dotfiles --apply
+/home/linuxbrew/.linuxbrew/bin/chezmoi init phault/dotfiles --apply
 
 echo "Installing fonts..."
 mkdir -p "$HOME/.fonts"
-curl -sSL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip > /tmp/JetBrainsMono.zip && \
-    unzip /tmp/JetBrainsMono.zip -d "$HOME/.fonts" "*.ttf" && \
-    rm /tmp/JetBrainsMono.zip
+curl -sSL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip >/tmp/JetBrainsMono.zip &&
+	unzip /tmp/JetBrainsMono.zip -d "$HOME/.fonts" "*.ttf" &&
+	rm /tmp/JetBrainsMono.zip
 
+# untested, PATH might not be up-to-date to run this
+# TODO: unnecessarily duplicated in setup_emacs.sh
+echo "Installing Node.js..."
+volta install node@lts
+volta install npm@latest
+
+echo "Installing global npm packages..."
+npm i -g prettier
+npm i -g stylelint
+npm i -g dockerfile-language-server-nodejs
+
+echo "Installing grip (markdown previewer) via pip..."
+python3 -m ensurepip
+pip3 install grip
+
+echo "Installing global dotnet tools..."
+dotnet tool install -g csharpier
+
+echo "Installing global Go modules..."
+go install github.com/jessfraz/dockfmt@latest
+go install github.com/x-motemen/gore/cmd/gore@latest
+go install github.com/stamblerre/gocode@latest
+go install golang.org/x/tools/cmd/godoc@latest
+go install golang.org/x/tools/cmd/goimports@latest
+go install golang.org/x/tools/cmd/gorename@latest
+go install golang.org/x/tools/cmd/guru@latest
+go install github.com/cweill/gotests/gotests@latest
+go install github.com/fatih/gomodifytags@latest
 
 echo "Setup complete."
--- exact_scripts/executable_setup_mac.sh
+++ exact_scripts/executable_setup_mac.sh
@@ -17,5 +17,31 @@ curl -sSL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBr
     unzip /tmp/JetBrainsMono.zip -d "$HOME/Library/Fonts" "*.ttf" && \
     rm /tmp/JetBrainsMono.zip
 
+# untested, PATH might not be up-to-date to run this
+echo "Installing Node.js..."
+volta install node@lts
+volta install npm@latest
+
+echo "Installing global npm packages..."
+npm i -g prettier
+npm i -g stylelint
+npm i -g dockerfile-language-server-nodejs
+
+echo "Installing grip (markdown previewer) via pip..."
+pip install grip
+
+echo "Installing global dotnet tools..."
+dotnet tool install -g csharpier
+
+echo "Installing global Go modules..."
+go install github.com/jessfraz/dockfmt@latest
+go install github.com/x-motemen/gore/cmd/gore@latest
+go install github.com/stamblerre/gocode@latest
+go install golang.org/x/tools/cmd/godoc@latest
+go install golang.org/x/tools/cmd/goimports@latest
+go install golang.org/x/tools/cmd/gorename@latest
+go install golang.org/x/tools/cmd/guru@latest
+go install github.com/cweill/gotests/gotests@latest
+go install github.com/fatih/gomodifytags@latest
 
 echo "Setup complete."
