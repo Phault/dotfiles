ebf8d2130fa05f7d5665c0996ee9bb038fedd377
Casper Lindschouw <casper@lindschouw.me>
2023-11-18 11:24:43 +0100

    disable integrated title bar for wezterm on windows

--- dot_config/wezterm/os_specific.lua
+++ dot_config/wezterm/os_specific.lua
@@ -4,6 +4,9 @@ return function(config)
 	if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 		config.default_prog = { "powershell.exe", "-NoLogo" }
 		config.font_size = 10
+
+		-- the integrated decorations really don't work well on windows
+		config.window_decorations = "TITLE | RESIZE"
 	end
 
 	return config
