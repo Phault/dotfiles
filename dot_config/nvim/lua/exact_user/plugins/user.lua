3bab4ec865076b508cf2a12f97188b40d0d30097
Casper Lindschouw <casper@lindschouw.me>
2023-12-09 20:35:41 +0100

    add test mappings
    they're a bit awkward due to the capital T but lowercase was taken :(

--- dot_config/nvim/lua/exact_user/plugins/user.lua
+++ dot_config/nvim/lua/exact_user/plugins/user.lua
@@ -1,22 +1,35 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
-  {
-    -- disabled by default on Windows, but several plugins expect it to be enabled
-    "mfussenegger/nvim-dap",
-    enabled = true,
-  },
-  {
-    "NoahTheDuke/vim-just",
-    event = { "BufReadPre", "BufNewFile" },
-    ft = { "\\cjustfile", "*.just", ".justfile" },
-  },
+	-- You can also add new plugins here as well:
+	-- Add plugins, the lazy syntax
+	-- "andweeb/presence.nvim",
+	-- {
+	--   "ray-x/lsp_signature.nvim",
+	--   event = "BufRead",
+	--   config = function()
+	--     require("lsp_signature").setup()
+	--   end,
+	-- },
+	{
+		-- disabled by default on Windows, but several plugins expect it to be enabled
+		"mfussenegger/nvim-dap",
+		enabled = true,
+	},
+	{
+		"NoahTheDuke/vim-just",
+		event = { "BufReadPre", "BufNewFile" },
+		ft = { "\\cjustfile", "*.just", ".justfile" },
+	},
+	{
+		"nvim-neotest/neotest",
+		opts = function(_, opts)
+			opts.summary = {
+				mappings = {
+					expand = { "<Tab>", "<CR>", "<2-LeftMouse>" },
+					expand_all = { "<S-Tab>", "e" },
+				},
+			}
+
+			return opts
+		end,
+	},
 }
